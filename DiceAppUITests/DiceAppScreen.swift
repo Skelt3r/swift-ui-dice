fe0c7f31fe4cc97b221d842a36e3690b0663ba38
Chris Allen <helterskelt3r@gmail.com>
2024-07-13 16:49:03 -0500

    Add UI tests

--- DiceAppUITests/DiceAppScreen.swift
+++ DiceAppUITests/DiceAppScreen.swift
@@ -5,9 +5,177 @@
 //  Created by Chris Allen on 7/10/24.
 //
 
+import SwiftUI
 import XCTest
 
-class Screen {
+class DiceAppScreen {
+    
+    // MARK: Structs
+    
+    enum Dice: String {
+        case d100 = "d100"
+        case d20 = "d20"
+        case d12 = "d12"
+        case d10 = "d10"
+        case d8 = "d8"
+        case d6 = "d6"
+        case d4 = "d4"
+    }
+    
+    // MARK: Elements
+    
     let app = XCUIApplication()
+    
+    private lazy var settingsButton = app.buttons["settingsButton"]
+    private lazy var darkModeToggle = app.switches["darkModeToggle"]
+    private lazy var colorMenuLabel = app.sheets.buttons["colorMenuLabel"]
+    private lazy var xButtonSettings = app.buttons["xButtonSettings"]
+    
+    private lazy var diceMenuLabel = app.staticTexts["diceMenuLabel"]
+    
+    private lazy var diceAmountResetButton = app.buttons["diceAmountResetButton"]
+    private lazy var diceAmountStepper = app.steppers["diceAmountStepper"]
+    private lazy var diceAmountStepperIncrement = diceAmountStepper.buttons["diceAmountStepper-Increment"]
+    private lazy var diceAmountStepperDecrement = diceAmountStepper.buttons["diceAmountStepper-Decrement"]
+    
+    private lazy var rollModifierResetButton = app.buttons["rollModifierResetButton"]
+    private lazy var rollModifierStepper = app.steppers["rollModifierStepper"]
+    private lazy var rollModifierStepperIncrement = rollModifierStepper.buttons["rollModifierStepper-Increment"]
+    private lazy var rollModifierStepperDecrement = rollModifierStepper.buttons["rollModifierStepper-Decrement"]
+    
+    private lazy var sumLabel = app.staticTexts["sumLabel"]
+    
+    private lazy var resultsListButton = app.buttons["resultsListButton"]
+    private lazy var xButtonResultsList = app.buttons["xButtonResultsList"]
+    
     private lazy var rollButton = app.buttons["rollButton"]
+    private lazy var inputLabel = app.staticTexts["inputLabel"]
+    
+    // MARK: Actions
+    
+    @discardableResult
+    func tapSettingsButton() -> Self {
+        settingsButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func toggleDarkMode() -> Self {
+        darkModeToggle.coordinate(
+            withNormalizedOffset: CGVector(dx: 0.9, dy: 0.5)
+        ).tap()
+        return self
+    }
+
+    @discardableResult
+    func selectPrimaryColor(_ option: Color) -> Self {
+        colorMenuLabel.tap()
+        app.buttons["\(option)ColorButton"].tap()
+        return self
+    }
+    
+    @discardableResult
+    func tapSettingsXButton() -> Self {
+        xButtonSettings.tap()
+        return self
+    }
+    
+    @discardableResult
+    func selectDiceType(_ option: Dice) -> Self {
+        diceMenuLabel.tap()
+        app.buttons["\(option)DiceButton"].tap()
+        return self
+    }
+    
+    @discardableResult
+    func increaseDiceAmount(by number: Int) -> Self {
+        for _ in 1...number {
+            diceAmountStepperIncrement.tap()
+        }
+        return self
+    }
+    
+    @discardableResult
+    func decreaseDiceAmount(by number: Int) -> Self {
+        for _ in 1...number {
+            diceAmountStepperDecrement.tap()
+        }
+        return self
+    }
+    
+    @discardableResult
+    func resetDiceAmount() -> Self {
+        diceAmountResetButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func resetRollModifier() -> Self {
+        rollModifierResetButton.tap()
+        return self
+    }
+    
+    @discardableResult
+    func increaseRollModifier(by number: Int) -> Self {
+        for _ in 1...number {
+            rollModifierStepperIncrement.tap()
+        }
+        return self
+    }
+    
+    @discardableResult
+    func decreaseRollModifier(by number: Int) -> Self {
+        for _ in 1...number {
+            rollModifierStepperDecrement.tap()
+        }
+        return self
+    }
+    
+    @discardableResult
+    func tapRollButton() -> Self {
+        rollButton.tap()
+        return self
+    }
+    
+    // MARK: Assertions
+    
+    @discardableResult
+    func validateDiceTypeLabel(_ expectedValue: Dice) -> Self {
+        XCTAssertEqual(diceMenuLabel.label, expectedValue.rawValue)
+        return self
+    }
+    
+    @discardableResult
+    func validateDiceAmountLabel(_ expectedValue: Int) -> Self {
+        XCTAssertEqual(diceAmountStepper.label, String(expectedValue))
+        return self
+    }
+    
+    @discardableResult
+    func validateRollModifierLabel(_ expectedValue: Int) -> Self {
+        XCTAssertEqual(rollModifierStepper.label, String(expectedValue))
+        return self
+    }
+    
+    @discardableResult
+    func validateDefaultSumLabel() -> Self {
+        XCTAssertEqual(sumLabel.label, "0")
+        return self
+    }
+    
+    @discardableResult
+    func validateActiveSumLabel() -> Self {
+        let diceType = Int(diceMenuLabel.label.replacingOccurrences(of: "d", with: ""))!
+        let diceAmount = Int(diceAmountStepper.label)!
+        let sum = Int(sumLabel.label)!
+        XCTAssertGreaterThan(sum, 0)
+        XCTAssertLessThanOrEqual(sum, diceType * diceAmount)
+        return self
+    }
+    
+    @discardableResult
+    func validateInputLabel(_ expectedValue: String) -> Self {
+        XCTAssertEqual(inputLabel.label, expectedValue)
+        return self
+    }
 }
--- DiceAppUITests/DiceAppUITests.swift
+++ DiceAppUITests/DiceAppUITests.swift
@@ -7,35 +7,63 @@
 
 import XCTest
 
-final class DiceAppUITests: XCTestCase {
-
+class DiceAppUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDownWithError() throws {
+        app.terminate()
+    }
+    
+    func test_rollSingleDice() throws {
+        DiceAppScreen()
+            .tapRollButton()
+            .validateActiveSumLabel()
+            .validateInputLabel("You rolled 1d20")
+    }
+    
+    func test_rollMultipleDice() throws {
+        DiceAppScreen()
+            .selectDiceType(.d6)
+            .validateDiceTypeLabel(.d6)
+            .increaseDiceAmount(by: 3)
+            .validateDiceAmountLabel(4)
+            .tapRollButton()
+            .validateActiveSumLabel()
+            .validateInputLabel("You rolled 4d6")
+    }
+    
+    func test_addRollModifier() throws {
+        DiceAppScreen()
+            .increaseRollModifier(by: 1)
+            .validateRollModifierLabel(1)
+            .tapRollButton()
+            .validateActiveSumLabel()
+            .validateInputLabel("You rolled 1d20+1")
+    }
+    
+    func test_resetDiceAmount() throws {
+        DiceAppScreen()
+            .increaseDiceAmount(by: 3)
+            .tapRollButton()
+            .resetDiceAmount()
+            .validateDiceAmountLabel(1)
+            .validateDefaultSumLabel()
+            .validateInputLabel("...")
+    }
+    
+    func test_resetRollModifier() throws {
+        DiceAppScreen()
+            .increaseRollModifier(by: 3)
+            .tapRollButton()
+            .resetRollModifier()
+            .validateRollModifierLabel(0)
+            .validateDefaultSumLabel()
+            .validateInputLabel("...")
     }
 }
