9db74368387349baaa4f734d11e1fd5bfa71253d
Chris Allen <helterskelt3r@gmail.com>
2024-07-14 14:23:38 -0500

    Color extension

---
+++ DiceApp/Color.swift
@@ -0,0 +1,33 @@
+//
+//  Color.swift
+//  DiceApp
+//
+//  Created by Chris Allen on 7/14/24.
+//
+
+import SwiftUI
+
+extension Color: RawRepresentable {
+    public init?(rawValue: String) {
+        guard let data = Data(base64Encoded: rawValue) else {
+            self = .black
+            return
+        }
+        
+        do {
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data) ?? .black
+            self = Color(color)
+        } catch {
+            self = .black
+        }
+    }
+
+    public var rawValue: String {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+        } catch {
+            return ""
+        }
+    }
+}
